9cc8fd8af43b984f5ea05a5085f97bf4148e545a
Ivan Rogovskiy <ivanrogovskiy@Ivans-MacBook-Pro-2.local>
2022-07-18 14:01:18 +0300

    cors handler fixed

--- backend/middlewares/corsHandler.js
+++ backend/middlewares/corsHandler.js
@@ -1,11 +1,23 @@
-module.exports.corsHandler = (req, res, next) => {
+module.exports.corsAccessHandler = (req, res, next) => {
     const allowedCors = [
         'https://api.ivanrg.mesto.nomorepartiesxyz.ru',
         'http://api.ivanrg.mesto.nomorepartiesxyz.ru',
-        'localhost:3000'
-    ]
-    if (allowedCors.includes(origin))
+        'localhost:3000',
+        'http://localhost:3001'
+    ];
+    const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+
     const { origin } = req.headers;
-    res.header('Access-Control-Allow-Origin', origin);
+    const { method } = req.headers;
+    if (allowedCors.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+        res.header('Access-Control-Allow-Headers', 'authorization, content-type');
+    }
+    const requestHeaders = req.headers['access-control-request-headers'];
+    if (method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+        res.header('Access-Control-Allow-Headers', requestHeaders);
+        return res.end();
+    }
     next();
 }
